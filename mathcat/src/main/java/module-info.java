5f4ca1f7679764519256db7961f1df6ed7825d03
zayan0p <zayanfaizal13@gmail.com>
2026-04-19 21:45:25 +1000

    Feat Gemini Api

--- mathcat/src/main/java/module-info.java
+++ mathcat/src/main/java/module-info.java
@@ -6,6 +6,9 @@ module com.mathcat.mathcat {
     requires com.dlsc.formsfx;
     requires org.kordamp.ikonli.javafx;
     requires com.almasb.fxgl.all;
+    requires java.net.http;
+    requires com.google.gson;
+    requires io.github.cdimascio.dotenv.java;
 
     opens com.mathcat.mathcat to javafx.fxml;
 
