1c37ea606e7a8f8d67208157ff7809a715bc2557
Alex <alexander.grossi@connect.qut.edu.au>
2026-04-20 21:06:49 +1000

    Added starter background and title effects

--- mathcat/src/main/java/com/mathcat/mathcat/HelloApplication.java
+++ mathcat/src/main/java/com/mathcat/mathcat/HelloApplication.java
@@ -3,6 +3,7 @@ package com.mathcat.mathcat;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 
@@ -11,6 +12,12 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
+        Font.loadFont(
+                getClass().getResourceAsStream("/com/mathcat/mathcat/PressStart2P-Regular.ttf"),
+                12
+        );
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 500);
         stage.setTitle("Hello!");
