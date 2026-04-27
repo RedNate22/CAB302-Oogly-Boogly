6d2bedd93c7a429eb13b6632ea03e99b7214d7c1
Nathan Pithie <nathan.pithie@gmail.com>
2026-04-28 00:20:47 +1000

    fix: added call to DatabaseManager to init db on app start, rename: HelloApplication to MathCatApp

--- mathcat/src/main/java/com/mathcat/mathcat/MathCatApp.java
+++ mathcat/src/main/java/com/mathcat/mathcat/MathCatApp.java
@@ -7,16 +7,18 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import com.mathcat.mathcat.database.DatabaseManager;
 
-public class HelloApplication extends Application {
+public class MathCatApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        DatabaseManager.initialiseDatabase(); // setup db, create tables
 
         Font.loadFont(getClass().getResourceAsStream(
                 "/com/mathcat/mathcat/assets/font/PressStart2P-Regular.ttf"), 12);
 
         FXMLLoader fxmlLoader = new FXMLLoader(
-                HelloApplication.class.getResource("/com/mathcat/mathcat/hello-view.fxml"));
+                MathCatApp.class.getResource("/com/mathcat/mathcat/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 500);
         stage.setTitle("MathCat");
         stage.setScene(scene);
