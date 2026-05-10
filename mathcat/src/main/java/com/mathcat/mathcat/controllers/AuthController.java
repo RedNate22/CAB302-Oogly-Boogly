7931467670868039aa14778a0a623113e9b84eeb
Alex <alexander.grossi@connect.qut.edu.au>
2026-05-10 11:59:03 +1000

    Password entry field is now PasswordField instead of TextField

--- mathcat/src/main/java/com/mathcat/mathcat/controllers/AuthController.java
+++ mathcat/src/main/java/com/mathcat/mathcat/controllers/AuthController.java
@@ -7,6 +7,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -23,7 +24,7 @@ import com.mathcat.mathcat.models.User;
 public class AuthController {
 
     @FXML private TextField usernameField;
-    @FXML private TextField passwordField;
+    @FXML private PasswordField passwordField;
     @FXML private TextField emailField;
     @FXML private Label error;
 
