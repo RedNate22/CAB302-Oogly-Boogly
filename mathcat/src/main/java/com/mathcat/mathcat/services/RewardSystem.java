e9681c946e77077474a40bb3c94ef75b0a95a592
Alex <alexander.grossi@connect.qut.edu.au>
2026-05-11 13:56:34 +1000

    Created reward system class and implemented baseXP return

---
+++ mathcat/src/main/java/com/mathcat/mathcat/services/RewardSystem.java
@@ -0,0 +1,37 @@
+package com.mathcat.mathcat.services;
+
+import com.mathcat.mathcat.dao.CatDAO;
+import com.mathcat.mathcat.models.Cat;
+import com.mathcat.mathcat.models.Difficulty;
+import com.mathcat.mathcat.models.Question;
+
+public class RewardSystem {
+    // prototype system
+    // READ DIFFICULTY TO BEGIN WITH
+    // AWARD USER WITH XP BASED ON DIFFICULTY (ITEM STUFF LATER)
+    // 3 SEPERATE METHODS FOR BONUS XP, NO AI, HAPPY CAR AND WELL FED
+    // NEEDS TO BE ABLE TO ACCESS CURRENT USERS XP AND LEVEL
+
+    public int baseXpReturn(Question question) {
+        int xp = 0;
+
+        if (question.getDifficulty() == Difficulty.EASY) {
+            xp = 10;
+            return xp;
+        }
+
+        if (question.getDifficulty() == Difficulty.MEDIUM) {
+            xp = 20;
+            return xp;
+        }
+
+        if (question.getDifficulty() == Difficulty.HARD) {
+            xp = 35;
+            return xp;
+        }
+        return xp;
+    }
+
+
+
+}
