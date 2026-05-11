a2f88b181a1b1778b313ca81baf8f1354513fbae
Alex <alexander.grossi@connect.qut.edu.au>
2026-05-11 14:43:40 +1000

    Javadoc comments

--- mathcat/src/main/java/com/mathcat/mathcat/services/RewardSystem.java
+++ mathcat/src/main/java/com/mathcat/mathcat/services/RewardSystem.java
@@ -5,13 +5,18 @@ import com.mathcat.mathcat.models.Cat;
 import com.mathcat.mathcat.models.Difficulty;
 import com.mathcat.mathcat.models.IQuestion;
 
+/**
+ * Manages the XP gain a user will receive upon successfully completing a question.
+ * This class contains rules for subsequent XP gain and returns it.
+ */
 public class RewardSystem {
-    // prototype system
-    // READ DIFFICULTY TO BEGIN WITH
-    // AWARD USER WITH XP BASED ON DIFFICULTY (ITEM STUFF LATER)
-    // 3 SEPERATE METHODS FOR BONUS XP, NO AI, HAPPY CAR AND WELL FED
-    // NEEDS TO BE ABLE TO ACCESS CURRENT USERS XP AND LEVEL
 
+    /**
+     * Returns the base XP a user will receive based on the difficulty of the question.
+     *
+     * @param question the current question
+     * @return the XP user will receive based on difficulty
+     */
     public static double baseXpReturn(IQuestion question) {
         double xp = 0;
 
@@ -32,6 +37,15 @@ public class RewardSystem {
         return xp;
     }
 
+    /**
+     * Returns the bonus XP a user will receive based on criteria of when and how the user answered the question.
+     * To receive full bonus XP, the user must be in surplus of 75 happiness, 25 fullness and has answered
+     * the question unassisted
+     *
+     * @param car the current user
+     * @param chatController the AI chatcontroller
+     * @return the bonus XP user will receive based on criteria
+     */
     public static double xpBonus(Cat car, ChatController chatController) {
         double bonus = 0;
 
@@ -49,6 +63,15 @@ public class RewardSystem {
         return bonus;
     }
 
+    /**
+     * Returns the total XP a user will receive based on the criteria of baseXP and bonusXP
+     *
+     * @param car the current user
+     * @param question the current question
+     * @param chatController the AI chatcontroller
+     *
+     * @return the total XP the user will receive
+     */
     public static double playerXpReturn(Cat car, IQuestion question, ChatController chatController) {
         double totalXp = 0;
 
