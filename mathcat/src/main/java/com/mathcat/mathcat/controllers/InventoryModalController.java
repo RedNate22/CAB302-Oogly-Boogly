d7651b1b2a2ed16f71fa6c93ad6805ae54de5acf
smolbebby <leo.vandesande@outlook.com>
2026-05-12 16:40:01 +1000

    feat(modal): added base functions for modal controller for inventory

--- mathcat/src/main/java/com/mathcat/mathcat/controllers/InventoryModalController.java
+++ mathcat/src/main/java/com/mathcat/mathcat/controllers/InventoryModalController.java
@@ -12,10 +12,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.image.*;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import java.io.IOException;
 
+/** 
+*
+*/
 public class InventoryModalController {
     private Cat cat; // needs to be scoped here to be accessible by onSubmit()
 
@@ -23,13 +27,71 @@ public class InventoryModalController {
     private Label petNameLabel;
 
     @FXML
+    private ImageView imageView;
+
+    private InventoryModalController inventoryModalController;
+
+    /**
+     * @param event
+     */
+    @FXML
     public void initialize() {
         cat = CatDAO.load(UserDAO.currentUser.getId());
         if (cat != null) {
             // load amount of items user has
-            }
         }
+    }
 
-    
+    /**
+     * @param event
+     * @return Stage
+     */
+    private static Stage getRoot(ActionEvent event) {
+        Node root = (Node) event.getSource();
+        return (Stage) root.getScene().getWindow();
+    }
+
+    /**
+     * @param event
+     * @return Stage
+     */
+    private static Stage getRoot(MouseEvent event) {
+        Node root = (Node) event.getSource();
+        return (Stage) root.getScene().getWindow();
+    }
+
+    /**
+     * @param event
+     * @throws IOException
+     */
+    @FXML
+    protected void changeCatColour(ActionEvent event) throws IOException {
+
+    }
+
+    /**
+     * @param event
+     * @throws IOException
+     */
+    @FXML
+    protected void changeCatAccessory(ActionEvent event) throws IOException {
+
+    }
+
+    /**
+     * @param event
+     * @throws IOException
+     */
+    @FXML
+    private void confirmSaveDetails(ActionEvent event) throws IOException {
+
+    }
+
+    /**
+     * @param inventoryModalController
+     */
+    public void setMainController(InventoryModalController inventoryModalController) {
+        this.inventoryModalController = inventoryModalController;
+    }
 
 }
